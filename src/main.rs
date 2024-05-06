6511151e8884ac9d928e3f491fcfaca5d6acb4c7
Abhijeeth Babu <abhijeethbabu@gmail.com>
2024-05-07 03:42:08 +0530

    clap branch finished

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,55 @@
 use clap::Parser;
+use colored::Colorize;
 
 #[derive(Parser)]
 struct Arguments {
-    #[clap(default_value="Meow")]
+    #[clap(default_value = "Meow")]
+    /// What does the cat say?
     message: String, // input message
-}
-fn main() {
-    let args = Arguments::parse();
-    let message = args.message;
-    
-    println!("{}", message);
-    println!(" \\");
-    println!("  \\");
-    let cat = std::fs::read_to_string("ascii_art/cat.txt")
-        .unwrap_or_else(|e| panic!("{}", e));
-    println!("{}", cat);
+
+    #[clap(short = 'd', long = "dead")]
+    /// Make the cat appear dead
+    dead: bool,
+
+    #[clap(short = 'f', long = "file")]
+    /// Import ascii art from file
+    catfile: Option<std::path::PathBuf>,
 }
 
+static DEFAULT_CAT: &str = "ascii_art/cat.txt";
+fn main() {
+    let args = Arguments::parse();
+    run(args);
+}
+
+fn run(args: Arguments) {
+    let message = args.message;
+
+    if message.to_lowercase() == "woof" {
+        eprintln!("Kitties don't bark! They mew!")
+    }
+
+    let mut cat;
+
+    match args.catfile {
+        Some(path) => {
+            cat = std::fs::read_to_string(path).unwrap_or_else(|e| panic!("{}", e));
+        }
+        None => {
+            cat = std::fs::read_to_string(DEFAULT_CAT).unwrap_or_else(|e| panic!("{}", e));
+
+            if args.dead {
+                cat = cat.replace("{eye}", &"\u{02e3}".red().bold().to_string());
+                cat = cat.replace("{heart}", "\u{1f480}");
+            } else {
+                cat = cat.replace("{eye}", &"•".green().bold().to_string());
+                cat = cat.replace("{heart}", &"♡".red().to_string());
+            }
+        }
+    }
+
+    println!("{}", message.bright_yellow().underline().on_purple());
+    println!(" \\");
+    println!("  \\");
+    println!("{}", cat);
+}
