6ab243b005d1bb05bb53cb4d70e5c242516b3737
Abhijeeth Babu <abhijeethbabu@gmail.com>
2024-05-06 23:07:21 +0530

    added ascii art

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Arguments {
-    message: String // input message
+    #[clap(default_value="Meow")]
+    message: String, // input message
 }
 fn main() {
     let args = Arguments::parse();
     let message = args.message;
+    
     println!("{}", message);
     println!(" \\");
     println!("  \\");
-    println!("     /\\_/\\");
-    println!("    ( o o )");
-    println!("    =( I )=");
+    let cat = std::fs::read_to_string("ascii_art/cat.txt")
+        .unwrap_or_else(|e| panic!("{}", e));
+    println!("{}", cat);
 }
+
