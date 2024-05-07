ad14c2497220ed32923275b5f303446fafb199c6
Abhijeeth Babu <abhijeethbabu@gmail.com>
2024-05-07 14:55:51 +0530

    before adding anyhow

--- src/main.rs
+++ src/main.rs
@@ -17,23 +17,19 @@ struct Arguments {
 }
 
 static DEFAULT_CAT: &str = " ∧,,,∧\n({eye} · {eye})\n/    づ{heart} ";
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Arguments::parse();
-    run(args);
-}
-
-fn run(args: Arguments) {
     let message = args.message;
 
     if message.to_lowercase() == "woof" {
-        eprintln!("Kitties don't bark! They mew!")
+        eprintln!("Kitties don't bark! They meow!")
     }
 
-    let mut cat;
+    let mut cat: String;
 
     match args.catfile {
         Some(path) => {
-            cat = std::fs::read_to_string(path).unwrap_or_else(|e| panic!("{}", e));
+            cat = std::fs::read_to_string(path)?;
         }
         None => {
             cat = DEFAULT_CAT.to_string();
@@ -52,4 +48,6 @@ fn run(args: Arguments) {
     println!(" \\");
     println!("  \\");
     println!("{}", cat);
+
+    Ok(())
 }
